c35d8763ac425637e1eefc0db2fcbaa1a2221fad
Devon Weller <dweller@devonweller.com>
2015-05-21 15:52:49 -0500

    Handle only .env.example file.

--- App/Environment.php
+++ App/Environment.php
@@ -14,7 +14,15 @@ class Environment
     public static function init($base_path) {
         define('BASE_PATH', realpath($base_path));
 
-        Dotenv::load(BASE_PATH);
+        $env_file = '.env';
+        if (!file_exists(BASE_PATH.'/.env')) {
+            if (file_exists(BASE_PATH.'/.env.example')) {
+                $env_file = '.env.example';
+            } else {
+                throw new Exception(".env or .env.example file not found", 1);
+            }
+        }
+        Dotenv::load(BASE_PATH, $env_file);
 
         Log::initMonolog(getenv('LOG_PATH'));
     }
